23f4c1fbeb8a96e92f74753706ccbaef39922c01
aamoghS <aamoghsawantt@gmail.com>
2025-08-13 18:48:27 -0400

    added my intro:gurt

--- src/app/cool/page.tsx
+++ src/app/cool/page.tsx
@@ -1,13 +1,17 @@
 export default function Kewl() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-primary text-white gap-5">
-      {/* 
-      Write something unique about you here! 
-      It could be a club you're part of, a weird skill you have, or something special that happened to you.
-      Feel free to put links, images, whatever! 
-      Don't worry about styling- we aren't grading you on this- it's just to get to know you better! :) 
+    <div className="min-h-screen flex flex-col justify-center items-center bg-primary text-white gap-5 px-4 text-center">
+      {/*
+        Write something unique about you here! 
+        It could be a club you're part of, a weird skill you have, or something special that happened to you.
+        Feel free to put links, images, whatever! 
+        Don't worry about styling- we aren't grading you on this- it's just to get to know you better! :) 
       */}
-      😎😎😎
+      <h1 className="text-6xl font-extrabold">yo: GURTTTTTTTTTTTT</h1>
+      <p className="text-xl max-w-xl">
+        Hi! I'm Aamogh Sawant, a transfer student from UCF. I am really excited to join Bits of Good, 
+        as I love contributing to the people around me and always helping out.
+      </p>
     </div>
   );
 }
